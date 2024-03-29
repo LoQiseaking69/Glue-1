7e36b6a367fe009d321c99a712ab7a1961d5ef33
Ant <130005196+LoQiseaking69@users.noreply.github.com>
2024-03-29 09:40:23 -0400

    Update parser.rs

--- Syntax Definition/Semantics Analysis/parser.rs
+++ Syntax Definition/Semantics Analysis/parser.rs
@@ -1,10 +1,10 @@
 use nom::{
     branch::alt,
     bytes::complete::tag,
-    character::complete::{char, digit1, multispace0, multispace1},
-    combinator::{map, opt, recognize},
-    multi::{many0, separated_list0},
-    sequence::{delimited, pair, preceded, terminated, tuple},
+    character::complete::{alpha1, alphanumeric1, char, digit1, multispace0, multispace1},
+    combinator::{map, recognize},
+    multi::{many0},
+    sequence::{delimited, pair, tuple},
     IResult,
 };
 
@@ -27,10 +27,8 @@ enum BinOp {
 }
 
 fn parse_identifier(input: &str) -> IResult<&str, Expr> {
-    map(recognize(pair(
-        alpha1,
-        many0(alt((alphanumeric1, char('_'))),
-    ))), |id: &str| Expr::Identifier(id.to_string()))(input)
+    map(recognize(pair(alpha1, many0(alt((alphanumeric1, char('_')))))),
+        |id: &str| Expr::Identifier(id.to_string()))(input)
 }
 
 fn parse_number(input: &str) -> IResult<&str, Expr> {
@@ -38,10 +36,8 @@ fn parse_number(input: &str) -> IResult<&str, Expr> {
 }
 
 fn parse_rune(input: &str) -> IResult<&str, Expr> {
-    map(
-        delimited(char('['), recognize(many0(char('.'))), char(']')),
-        |rune: &str| Expr::Rune(rune.to_string()),
-    )(input)
+    map(delimited(char('['), recognize(many0(char('.'))), char(']')),
+        |rune: &str| Expr::Rune(rune.to_string()))(input)
 }
 
 fn parse_primary(input: &str) -> IResult<&str, Expr> {
@@ -63,64 +59,52 @@ fn parse_bin_op(input: &str) -> IResult<&str, BinOp> {
 }
 
 fn parse_expr(input: &str) -> IResult<&str, Expr> {
-    let (input, expr) = parse_primary(input)?;
-    let (input, bin_ops) = many0(pair(parse_bin_op, parse_primary))(input)?;
-
-    let mut result = expr;
-    for (op, operand) in bin_ops {
-        result = Expr::BinOp(Box::new(result), op, Box::new(operand));
-    }
-
-    Ok((input, result))
-}
-
-fn parse_assignment(input: &str) -> IResult<&str, Expr> {
-    let (input, _) = multispace0(input)?;
-    let (input, identifier) = parse_identifier(input)?;
-    let (input, _) = multispace0(input)?;
-    let (input, _) = tag("=")(input)?;
-    let (input, _) = multispace0(input)?;
-    let (input, expr) = parse_expr(input)?;
-
-    Ok((
-        input,
-        Expr::Assignment(identifier.get_identifier(), Box::new(expr)),
-    ))
+    let (input, primary) = parse_primary(input)?;
+    many0(tuple((parse_bin_op, parse_primary)))(input)
+        .map(|(next_input, list)| {
+            let expr = list.into_iter().fold(primary, |acc, (op, val)| {
+                Expr::BinOp(Box::new(acc), op, Box::new(val))
+            });
+            (next_input, expr)
+        })
 }
 
 fn parse_if(input: &str) -> IResult<&str, Expr> {
     let (input, _) = tag("HORUS")(input)?;
-    let (input, _) = multispace1(input)?;
-    let (input, condition) = parse_expr(input)?;
-    let (input, _) = multispace0(input)?;
-    let (input, _) = tag(":")(input)?;
-    let (input, _) = multispace0(input)?;
-    let (input, if_branch) = many0(terminated(parse_statement, multispace0))(input)?;
+    let (input, condition) = delimited(multispace1, parse_expr, multispace0)(input)?;
+    let (input, if_branch) = many0(parse_statement)(input)?;
     let (input, _) = tag("PYRAMID")(input)?;
-    let (input, _) = multispace0(input)?;
-    let (input, else_branch) = many0(terminated(parse_statement, multispace0))(input)?;
+    let (input, else_branch) = many0(parse_statement)(input)?;
 
-    Ok((
-        input,
-        Expr::If(Box::new(condition), if_branch, else_branch),
-    ))
+    Ok((input, Expr::If(Box::new(condition), if_branch, else_branch)))
+}
+
+fn parse_assignment(input: &str) -> IResult<&str, Expr> {
+    let (input, id) = parse_identifier(input)?;
+    let (input, _) = tag("=")(input)?;
+    let (input, expr) = parse_expr(input)?;
+
+    Ok((input, Expr::Assignment(id.get_identifier(), Box::new(expr))))
 }
 
 fn parse_statement(input: &str) -> IResult<&str, Expr> {
-    alt((parse_assignment, parse_if, parse_expr))(input)
+    alt((
+        parse_assignment,
+        parse_if,
+        parse_expr,
+    ))(input)
 }
 
 fn main() {
     let code = r#"
-VALKYRIE x = 5
-HORUS if x > 0:
-    ENIGMA y = x + 1
-PYRAMID else:
-    ENIGMA y = x - 1
-"#;
+    HORUS x > 0 :
+        VALKYRIE y = x + 1
+    PYRAMID :
+        VALKYRIE y = x - 1
+    "#;
 
-    let (_, statements) = many0(terminated(parse_statement, multispace0))(code).unwrap();
-
-    println!("{:?}", statements);
+    match many0(parse_statement)(code) {
+        Ok((_, statements)) => println!("Parsed Statements: {:?}", statements),
+        Err(err) => println!("Error parsing code: {:?}", err),
+    }
 }
-
